8c2f3b8bb6c06b9dcd0e4e2f04b3d34205c5c1d2
Matheus Ricardo Carvalho Silva <matheus.ricardo.cs@gmail.com>
2024-03-06 16:27:24 -0300

    whatsapp e cronjob

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,46 @@
-import express from 'express'
+import express from "express";
 import prisma from "./utils/prisma";
+import cron from "node-cron";
+import { create, Whatsapp } from "venom-bot";
+const app = express();
+app.use(express.json());
 
-const app = express()
-app.use(express.json())
+const port = process.env.PORT ?? 4000;
 
-const port = process.env.PORT ?? 4000
+cron.schedule("* * * * *", () => {
+  console.log("testei");
+});
 
-app.get('/sensores', async (request, response) => {
-    const sensores = await prisma.sensor.findMany()
-    return response.json(sensores)
-  })
-  
-  app.post('/sensores', async (request, response) => {
-    const { nome, valor } = request.body
-    const sensor = await prisma.sensor.create({
-      data: {
-        nome,
-        valor,
-      },
-    })
-    return response.json(sensor)
-  })
-  
-  app.listen(port, () => console.log('Server is running on port ', port))
+async function connectToWhatsApp() {
+  const client: Whatsapp = await create({
+    session: "GPZAP",
+    disableWelcome: true,
+  });
+
+  // Evento para quando uma mensagem é recebida
+  client.onMessage(async (message) => {
+    console.log("Mensagem recebida:", message.body);
+    await client.sendText(message.from, "Oi! Como posso ajudar?");
+  });
+}
+
+// Inicie a conexão
+connectToWhatsApp();
+
+app.get("/sensores", async (request, response) => {
+  const sensores = await prisma.sensor.findMany();
+  return response.json(sensores);
+});
+
+app.post("/sensores", async (request, response) => {
+  const { nome, valor } = request.body;
+  const sensor = await prisma.sensor.create({
+    data: {
+      nome,
+      valor,
+    },
+  });
+  return response.json(sensor);
+});
+
+app.listen(port, () => console.log("Server is running on port ", port));
